e3d768bb82f392d101a32437084e036171254780
liang guo <liang.guo@ensto.com>
2021-04-26 11:45:58 +0300

    archive

---
+++ precache-manifest.bf56e109d821aa6c8c464bc78fa68c4f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "1ca4a4ede7909cf2fddf",
+    "url": "/static/js/main.1ca4a4ed.chunk.js"
+  },
+  {
+    "revision": "32636255264115f60c6f",
+    "url": "/static/js/2.32636255.chunk.js"
+  },
+  {
+    "revision": "1ca4a4ede7909cf2fddf",
+    "url": "/static/css/main.5c4a2b5b.chunk.css"
+  },
+  {
+    "revision": "7c60e9ff1c1944b36340ee90797b3b32",
+    "url": "/index.html"
+  }
+];
